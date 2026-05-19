386b5d7c41a81118b8d6c06ee75f88bfaf82bf75
idaferiani1915 <idaferiani1915@gmail.com>
2026-05-19 10:09:42 +0700

    Buat Halaman Login Fixes #1

---
+++ lib/idaferiani.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IdaFeriani extends StatefulWidget {
+  const IdaFeriani({super.key});
+
+  @override
+  State<IdaFeriani> createState() => _IdaFerianiState();
+}
+
+class _IdaFerianiState extends State<IdaFeriani> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
