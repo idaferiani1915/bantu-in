de71f3c947e6121c9bc520f156c6b88e46c8c870
idaferiani1915 <idaferiani1915@gmail.com>
2026-05-19 09:46:01 +0700

    home

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:bantu_in/main.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
